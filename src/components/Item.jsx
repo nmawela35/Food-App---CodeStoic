6f8bd0fd576ee2300665c89bfc6d34361deb9ed6
Christopher Mawela <christopher.mawela@gmail.com>
2024-09-04 09:15:52 +0200

    Basic Food app complete

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import styles from './item.module.css'
 
 function Item({item}) {
   return (
     <div>
-         <div>
-            <img src={`https://spoonacular.com/cdn/ingredients_100x100/`+item.image} alt="" />
-            <h3>{item.name}</h3>
-            <h3>{item.amount} {item.unit}</h3>
-          </div>
+        <div className={styles.itemContainer}>
+            <div className={styles.imageContainer}>
+            <img className={styles.image} src={`https://spoonacular.com/cdn/ingredients_100x100/`+item.image} alt="" />
+            </div>
+            <div className={styles.nameContainer}>
+                <div className={styles.name}>{item.name}</div>
+                <div className={styles.amount}>{item.amount} {item.unit}</div>
+            </div>
+        </div>
     </div>
+
   )
 }
 
