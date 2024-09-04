3be0ae2cbb36a5d5bc9293d27713853df4fa3036
Christopher Mawela <christopher.mawela@gmail.com>
2024-09-04 08:19:02 +0200

    Item separated from item list

---
+++ src/components/Item.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Item({item}) {
+  return (
+    <div>
+         <div>
+            <img src={`https://spoonacular.com/cdn/ingredients_100x100/`+item.image} alt="" />
+            <h3>{item.name}</h3>
+            <h3>{item.amount} {item.unit}</h3>
+          </div>
+    </div>
+  )
+}
+
+export default Item
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,13 +1,10 @@
+import Item from "./Item"
 
 function ItemList({food,isLoading}) {
   return (
     <div>
             {isLoading ? <p>Loading</p>:food.extendedIngredients && food.extendedIngredients.map((item) => (
-          <div>
-            <img src={`https://spoonacular.com/cdn/ingredients_100x100/`+item.image} alt="" />
-            <h3>{item.name}</h3>
-            <h3>{item.amount} {item.unit}</h3>
-          </div>
+         <Item item={item}/>
         ))}
 
     </div>
